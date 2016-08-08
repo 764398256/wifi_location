0b6d558ecdf30a619691406c87cfd41f3a139c2e
Han Shen <thushenhan@gmail.com>
2016-08-08 20:50:43 +0800

    get histogram

--- scripts/process/exp6/ACE/ace.m
+++ scripts/process/exp6/ACE/ace.m
@@ -8,44 +8,47 @@ mat_folder=strcat(data_path,'mat/');
 % extract main features
 empty_file=dir(strcat(file_folder,'m*_empty.mat'));
 
-stand_file=dir(strcat(file_folder,'stand/*.mat'));
-map = [-1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        -1, 1, -1, 1, -1, 1, -1 ...\
-        1, 1, 1, 1, 1, 1, 1 ...\
-        1, 1, 1, 1, 1, 1, 1 ...\
+stand_file=dir(strcat(file_folder,'*.mat'));
+map = [-1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        -1, 1, -1, 1, -1, 1, -1;...\
+        -1, 1, -1, 1, -1, 1, -1; ...\
+        1, 1, 1, 1, 1, 1, 1; ...\
+        1, 1, 1, 1, 1, 1, 1; ...\
         1, 1, -1, -1, -1, 1, 1 ...\
 ];
 
 [row, col] = size(map);
-voxel_num = sum(map == 1);
+voxel_num = sum(sum(map == 1));
 pos = find(map == 1);
 index = [mod(pos + row - 1, row) + 1, floor((pos + row - 1)/row)];
-sample_len = 2000;
+sample_len = 200;
 stream_num = 3;
 subcarrier_num = 30;
 M = zeros(voxel_num,stream_num, subcarrier_num,  sample_len*2);
 
 for i= 1:numel(stand_file)
     fname = stand_file(i).name;
-    tag = str2double(regexpi(fname, '[\d+]*', 'match');
-    load(fname);
+    disp(fname);
+    tag = str2double(regexpi(fname, '[\d+]*', 'match'));
+    load(strcat(file_folder,fname));
     voxel_id = find(ismember(index, [tag(2), tag(3)], 'rows'));
     h_avg_amp = amplitude(sample_csiM(:,:,:,1:sample_len));
     static = length(strfind(fname, 'static'));
-    M( voxel_id, tag(1), :, ( static )* sample_len + 1: (staic + 1) *sample_len ) = h_avg_amp;
+    M( voxel_id, tag(1), :, ( static )* sample_len + 1: (static + 1) *sample_len ) = h_avg_amp;
 end
-M = squeeze(mean(M, 3));
+M = mean(M, 3);
 histo = fingerprint_builder(M); 
+histo_file = char(strcat(mat_folder,'histo.mat' ));
+save(histo_file, 'histo');
 
 
--- scripts/process/exp6/ACE/fingerprint_builder.m
+++ scripts/process/exp6/ACE/fingerprint_builder.m
@@ -6,23 +6,23 @@ srange = [-30, 30];
 step = 0.5;
 histo = zeros(voxel_num, stream_num* dim, 2, (srange(2) - srange(1))/step);
 %RSS Value Range
-edges = [srange(1)+step:step:srange(2)];
+edges = [srange(1):step:srange(2)];
 
 % Gussian Filter
 sigma = 5;
-size = 30;
-x = linspace(-size / 2, size / 2, size);
+rsize = 30;
+x = linspace(-rsize / 2, rsize / 2, rsize);
 gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
 gaussFilter = gaussFilter / sum (gaussFilter); % normalize
 
 for i = 1: stream_num* dim
-    s = floor(i/dim) + 1;
-    d = mod(i,dim) + 1;
+    s = floor( ( i+dim - 1)/dim) ;
+    d = mod(i + dim - 1,dim) + 1;
     for j = 1: voxel_num
-        h=histogram(M(j, s,d , :), edges, 'Normalization','probability');
-        ho= histogram( reshape( M([1: (j-1), (j+1):end], s, d:  ), sample_len*(voxel_num-1) ,1) , edges, 'Normalization','probability');
-        histo(i, j, 1, :)  = conv (h.Value, gaussFilter, 'same');
-        histo(i, j, 2, :)  = conv (ho.Value, gaussFilter, 'same');
+        [h,~]=histcounts(M(j, s,d , :), edges, 'Normalization','probability');
+        histo(i, j, 1, :)  = conv (h, gaussFilter, 'same')';
+        [ho,~]= histcounts( reshape( M([1: (j-1), (j+1):end], s, d,:  ), sample_len*(voxel_num-1) ,1) , edges, 'Normalization','probability');
+        histo(i, j, 2, :)  = conv (ho, gaussFilter, 'same')';
     end
 end
 %fingerprint = reshape(histo, stream_num* dim*voxel_num*2*(srange(2) - srange(1))/step );
