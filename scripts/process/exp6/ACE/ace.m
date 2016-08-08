79bda1dd64928845e378a0cf59114e7fdbc8a3b8
shenh10 <thushenhan@gmail.com>
2016-08-08 16:56:57 +0800

    preprocess some data

--- scripts/process/exp6/ACE/ace.m
+++ scripts/process/exp6/ACE/ace.m
@@ -1,10 +1,51 @@
 close all; 
 clear all;
-data_path='../../../data/exp5/';
+data_path='../../../data/exp6/';
 file_folder=strcat(data_path,'mat/');
 figure_folder=strcat(data_path, 'figure/');
 mat_folder=strcat(data_path,'mat/');
 
 % extract main features
 empty_file=dir(strcat(file_folder,'m*_empty.mat'));
-static_file=dir(strcat(file_folder,'m*one_stand_left_mid.mat'));
+
+stand_file=dir(strcat(file_folder,'stand/*.mat'));
+map = [-1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        -1, 1, -1, 1, -1, 1, -1 ...\
+        1, 1, 1, 1, 1, 1, 1 ...\
+        1, 1, 1, 1, 1, 1, 1 ...\
+        1, 1, -1, -1, -1, 1, 1 ...\
+];
+
+[row, col] = size(map);
+voxel_num = sum(map == 1);
+pos = find(map == 1);
+index = [mod(pos + row - 1, row) + 1, floor((pos + row - 1)/row)];
+sample_len = 2000;
+stream_num = 3;
+subcarrier_num = 30;
+M = zeros(voxel_num,stream_num, subcarrier_num,  sample_len*2);
+
+for i= 1:numel(stand_file)
+    fname = stand_file(i).name;
+    tag = str2double(regexpi(fname, '[\d+]*', 'match');
+    load(fname);
+    voxel_id = find(ismember(index, [tag(2), tag(3)], 'rows'));
+    h_avg_amp = amplitude(sample_csiM(:,:,:,1:sample_len));
+    static = length(strfind(fname, 'static'));
+    M( voxel_id, tag(1), :, ( static )* sample_len + 1: (staic + 1) *sample_len ) = h_avg_amp;
+end
+M = squeeze(mean(M, 3));
+histo = fingerprint_builder(M); 
+
+
--- scripts/process/exp6/ACE/fingerprint_builder.m
+++ scripts/process/exp6/ACE/fingerprint_builder.m
@@ -1,10 +1,10 @@
-function [histo,fingerprint] = fingerprint_builder( M )
-%FINGERPRINT_BUILDER 此处显示有关此函数的摘要
-%   此处显示详细说明
-[stream_num, dim , sample_len, voxel_num] = size(M);
+function [histo] = fingerprint_builder( M )
+% M: multiple dimension matrix containing data of [ streams, subcarriers, voxel_num, sample_len ]
+%   
+[voxel_num, stream_num, dim ,sample_len] = size(M);
 srange = [-30, 30];
 step = 0.5;
-histo = zeros(stream_num* dim, voxel_num, 2, (srange(2) - srange(1))/step);
+histo = zeros(voxel_num, stream_num* dim, 2, (srange(2) - srange(1))/step);
 %RSS Value Range
 edges = [srange(1)+step:step:srange(2)];
 
@@ -15,16 +15,16 @@ x = linspace(-size / 2, size / 2, size);
 gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
 gaussFilter = gaussFilter / sum (gaussFilter); % normalize
 
-for i = 1: stream_num*num
+for i = 1: stream_num* dim
     s = floor(i/dim) + 1;
     d = mod(i,dim) + 1;
     for j = 1: voxel_num
-    h=histogram(M(s,d , :, j), edges, 'Normalization','probability');
-    ho= histogram( reshape( M(s,d, [1: j-1, j+1:end], : ), sample_len*(voxel_num-1) ,1) , edges, 'Normalization','probability');
-    histo(i, j, 1, :)  = conv (h.Value, gaussFilter, 'same');
-    histo(i, j, 2, :)  = conv (ho.Value, gaussFilter, 'same');
+        h=histogram(M(j, s,d , :), edges, 'Normalization','probability');
+        ho= histogram( reshape( M([1: (j-1), (j+1):end], s, d:  ), sample_len*(voxel_num-1) ,1) , edges, 'Normalization','probability');
+        histo(i, j, 1, :)  = conv (h.Value, gaussFilter, 'same');
+        histo(i, j, 2, :)  = conv (ho.Value, gaussFilter, 'same');
     end
 end
-fingerprint = reshape(histo, stream_num* dim*voxel_num*2*(srange(2) - srange(1))/step );
+%fingerprint = reshape(histo, stream_num* dim*voxel_num*2*(srange(2) - srange(1))/step );
 end
 
